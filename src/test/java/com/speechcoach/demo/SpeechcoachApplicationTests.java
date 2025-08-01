75d700a543791d3bb5b4eb5cb3b8b10b1a910804
Brady Vo <bradylvo@gmail.com>
2025-08-01 17:12:17 -0500

    initial commit

---
+++ src/test/java/com/speechcoach/demo/SpeechcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speechcoach.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
