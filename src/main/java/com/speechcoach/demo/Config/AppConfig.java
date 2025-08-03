3f90ed8087220349d139071fdc6ed142b49a1b58
Brady Vo <bradylvo@gmail.com>
2025-08-03 16:09:05 -0500

    create authenticationmanager bean

--- src/main/java/com/speechcoach/demo/Config/AppConfig.java
+++ src/main/java/com/speechcoach/demo/Config/AppConfig.java
@@ -2,11 +2,15 @@ package com.speechcoach.demo.Config;
 
 import com.speechcoach.demo.Repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class AppConfig {
     private final UserRepository userRepository;
 
@@ -26,4 +30,9 @@ public class AppConfig {
     }
 
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws  Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
