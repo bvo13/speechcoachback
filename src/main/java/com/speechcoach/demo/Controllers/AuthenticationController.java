7d9db72eb730c38e615568d6cf829ec7e9848259
Brady Vo <bradylvo@gmail.com>
2025-08-03 16:37:36 -0500

    create register endpoint

--- src/main/java/com/speechcoach/demo/Controllers/AuthenticationController.java
+++ src/main/java/com/speechcoach/demo/Controllers/AuthenticationController.java
@@ -1,7 +1,53 @@
 package com.speechcoach.demo.Controllers;
 
+import com.speechcoach.demo.DTOs.CreateUserDto;
+import com.speechcoach.demo.Services.AuthenticationService;
+import com.speechcoach.demo.Util.AuthenticationResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody CreateUserDto createUserDto, HttpServletResponse httpServletResponse){
+        AuthenticationResponse authenticationResponse = authenticationService.register(createUserDto);
+        ResponseCookie accessCookie = ResponseCookie.from("access_token", authenticationResponse.getAccessToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(15))
+                .sameSite("Strict")
+                .build();
+        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", authenticationResponse.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofDays(7))
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok().headers(httpHeaders ->{
+            httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
+            httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
+
+                }
+                ).build();
+
+    }
 }
