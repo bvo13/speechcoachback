bab733e99fbb066e1d9598012b27e00f4bfa8eaf
Brady Vo <bradylvo@gmail.com>
2025-08-03 15:44:04 -0500

    implement security filter chain with authentication filter included

--- src/main/java/com/speechcoach/demo/Config/SecurityConfig.java
+++ src/main/java/com/speechcoach/demo/Config/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.speechcoach.demo.Config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.nio.file.AccessDeniedException;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
+    private final AuthenticationFilter authenticationFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity.cors(Customizer.withDefaults())
+                .csrf(csrf-> csrf.disable())
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception->
+                        exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                                    response.setStatus(HttpStatus.FORBIDDEN.value());
+                                    response.setContentType("application/json");
+                                    response.getWriter().write("{\"error\": \"Access Denied\"}");
+                                }))
+                                .build();
+
+    }
 }
