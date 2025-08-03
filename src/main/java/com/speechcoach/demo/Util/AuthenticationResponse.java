b44a08bdcf06ca24872733349081d4f88ae32820
Brady Vo <bradylvo@gmail.com>
2025-08-03 16:36:51 -0500

    create AuthenticationResponse for register method return

---
+++ src/main/java/com/speechcoach/demo/Util/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.speechcoach.demo.Util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String accessToken;
+    private String refreshToken;
+}
