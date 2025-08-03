a927f660370ff19b476d09f0f5e95059b754c230
Brady Vo <bradylvo@gmail.com>
2025-08-03 17:02:34 -0500

    UserEntity implement UserDetails for authentication setup

--- src/main/java/com/speechcoach/demo/Entities/UserEntity.java
+++ src/main/java/com/speechcoach/demo/Entities/UserEntity.java
@@ -1,10 +1,15 @@
 package com.speechcoach.demo.Entities;
 
+import com.speechcoach.demo.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -12,7 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 @Table(name="users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
     private Long id;
@@ -22,6 +27,8 @@ public class UserEntity {
     private String username;
 
     private String password;
+
+    private Role role;
 
     public UserEntity(String name, String username, String password){
         this.name=name;
@@ -36,5 +43,28 @@ public class UserEntity {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
