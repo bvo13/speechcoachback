784765090a0d5c19b1dff338e6027ccdbef7f986
Brady Vo <bradylvo@gmail.com>
2025-08-03 16:38:36 -0500

    create endpoint to retrieve current user

--- src/main/java/com/speechcoach/demo/Controllers/UserController.java
+++ src/main/java/com/speechcoach/demo/Controllers/UserController.java
@@ -4,19 +4,24 @@ import com.speechcoach.demo.DTOs.UserResponseDto;
 import com.speechcoach.demo.DTOs.CreateUserDto;
 import com.speechcoach.demo.Entities.UserEntity;
 import com.speechcoach.demo.Mappers.UserMapper;
+import com.speechcoach.demo.Services.AuthenticationService;
 import com.speechcoach.demo.Services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
-
-    public UserController(UserService userService, UserMapper userMapper) {
+    private final AuthenticationService authenticationService;
+    public UserController(UserService userService, UserMapper userMapper, AuthenticationService authenticationService) {
         this.userService = userService;
         this.userMapper = userMapper;
+        this.authenticationService = authenticationService;
     }
     @PostMapping("/users")
     public ResponseEntity<UserResponseDto> createUser(@RequestBody CreateUserDto createUserDto){
@@ -25,6 +30,18 @@ public class UserController {
         return new ResponseEntity<>(userMapper.mapTo(savedUser), HttpStatus.CREATED);
 
     }
+
+    @GetMapping("/users/me")
+    public ResponseEntity<UserResponseDto> getCurrentUser(){
+        Long userId = authenticationService.getCurrentUserId();
+        Optional<UserEntity> userEntity = userService.findUserById(userId);
+        return userEntity.map(user -> {
+            UserResponseDto userResponseDto = userMapper.mapTo(user);
+            return new ResponseEntity<>(userResponseDto,HttpStatus.OK);
+        }).orElse(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+    }
+
 
 
 }
