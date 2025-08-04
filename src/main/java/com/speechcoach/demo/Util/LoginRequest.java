42288016a7f5bc9619c6dcd3b29cc24428775949
Brady Vo <bradylvo@gmail.com>
2025-08-04 15:55:45 -0500

    create LoginRequest class

---
+++ src/main/java/com/speechcoach/demo/Util/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.speechcoach.demo.Util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
