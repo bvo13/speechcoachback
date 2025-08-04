cb45bfa2215e4396420a50fe90d6b3a4db8b466d
Brady Vo <bradylvo@gmail.com>
2025-08-04 15:56:14 -0500

    implement login and refresh endpoints in AuthenticationController

--- src/main/java/com/speechcoach/demo/Controllers/AuthenticationController.java
+++ src/main/java/com/speechcoach/demo/Controllers/AuthenticationController.java
@@ -3,8 +3,12 @@ package com.speechcoach.demo.Controllers;
 import com.speechcoach.demo.DTOs.CreateUserDto;
 import com.speechcoach.demo.Services.AuthenticationService;
 import com.speechcoach.demo.Util.AuthenticationResponse;
+import com.speechcoach.demo.Util.LoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
 import org.apache.coyote.Response;
+import org.hibernate.dialect.H2DurationIntervalSecondJdbcType;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -44,10 +48,56 @@ public class AuthenticationController {
 
         return ResponseEntity.ok().headers(httpHeaders ->{
             httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
-            httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
+            httpHeaders.add(HttpHeaders.SET_COOKIE,refreshCookie.toString());
 
                 }
                 ).build();
 
     }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response){
+        AuthenticationResponse authenticationResponse = authenticationService.login(loginRequest);
+        ResponseCookie accessCookie = ResponseCookie.from("access_token", authenticationResponse.getAccessToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(15))
+                .sameSite("Strict")
+                .build();
+        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", authenticationResponse.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(15))
+                .sameSite("Strict")
+                .build();
+        return ResponseEntity.ok().headers(httpHeaders -> {
+            httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
+            httpHeaders.add(HttpHeaders.SET_COOKIE,refreshCookie.toString());
+        }).build();
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(HttpServletRequest request, HttpServletResponse response){
+        AuthenticationResponse authenticationResponse = authenticationService.refresh(request);
+        ResponseCookie accessCookie = ResponseCookie.from("access_token", authenticationResponse.getAccessToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(15))
+                .sameSite("Strict")
+                .build();
+        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", authenticationResponse.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(15))
+                .sameSite("Strict")
+                .build();
+        return ResponseEntity.ok().headers(httpHeaders -> {
+            httpHeaders.add(HttpHeaders.SET_COOKIE,accessCookie.toString());
+            httpHeaders.add(HttpHeaders.SET_COOKIE,refreshCookie.toString());
+        }).build();
+    }
+
 }
